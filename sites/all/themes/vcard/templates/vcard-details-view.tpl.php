f395d1fb9095b643f1ff589517d20e058ec961d7
Senya <suharchuk.dima@gmail.com>
2018-02-09 10:38:07 +0000

    Refactored display of icons from svg to font. Deleted unnecessary more svg images

--- sites/all/themes/vcard/templates/vcard-details-view.tpl.php
+++ sites/all/themes/vcard/templates/vcard-details-view.tpl.php
@@ -5,22 +5,32 @@
         </div>
       <?php if ($editable): ?>
           <div class="actions">
-              <a href="my-vcards/<?php print $id; ?>/edit" class="edit"></a>
-              <a href="my-vcards/<?php print $id; ?>/clone" class="clone"></a>
-              <a href="my-vcards/<?php print $id; ?>/remove" class="remove"></a>
+              <a href="my-vcards/<?php print $id; ?>/edit">
+                  <i class="material-icons">border_color</i>
+              </a>
+              <a href="my-vcards/<?php print $id; ?>/clone">
+                  <i class="material-icons">content_copy</i>
+              </a>
+              <a href="my-vcards/<?php print $id; ?>/remove">
+                  <i class="material-icons">delete</i>
+              </a>
           </div>
       <?php endif; ?>
     </div>
     <div class="vcard-description">
         <div class="row">
             <div class="large-6 columns full-name">
-                <div class="icon"></div>
+                <div class="icon">
+                    <i class="material-icons">account_circle</i>
+                </div>
                 <div class="content">
                     <span><?php print $full_name; ?></span>
                 </div>
             </div>
             <div class="large-6 columns company">
-                <div class="icon"></div>
+                <div class="icon">
+                    <i class="material-icons">domain</i>
+                </div>
                 <div class="content">
                     <div><?php print $company; ?></div>
                     <div><?php print $position; ?></div>
@@ -29,7 +39,9 @@
         </div>
         <div class="row">
             <div class="large-6 columns communication">
-                <div class="icon"></div>
+                <div class="icon">
+                    <i class="material-icons">contact_phone</i>
+                </div>
                 <div class="content">
                     <div><?php print $phones; ?></div>
                     <div><?php print $emails; ?></div>
@@ -37,7 +49,9 @@
                 </div>
             </div>
             <div class="large-6 columns address">
-                <div class="icon"></div>
+                <div class="icon">
+                    <i class="material-icons">location_on</i>
+                </div>
                 <div class="content">
                     <div><?php print $address; ?></div>
                 </div>
