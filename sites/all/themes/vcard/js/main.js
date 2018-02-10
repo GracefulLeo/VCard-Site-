f1b6b9315ed321b79eb36ec96976646f07d3846a
Senya <suharchuk.dima@gmail.com>
2018-02-10 14:47:08 +0000

    Refactored main.js

--- sites/all/themes/vcard/js/main.js
+++ sites/all/themes/vcard/js/main.js
@@ -1,13 +1,20 @@
-jQuery(document).ready(function ($) {
-  // Initialize NanoScroller.
-  $(".nano").nanoScroller();
+(function ($) {
+  Drupal.behaviors.themeVCard = {
+    attach: function (context) {
+      // Initialize NanoScroller.
+      $(".nano", context).nanoScroller();
 
-  $('.dropdown-header').hover(function () {
-    $(this).find('.icon-bar:nth-child(2)').addClass('active');
-  }, function () {
-    $(this).find('.icon-bar:nth-child(2)').removeClass('active');
-  }).on('click', function () {
-    $('.dropdown-menu').toggle();
-    $(this).toggleClass('active');
-  })
-});
+      $('.dropdown-header', context).hover(function () {
+        $(this).find('.icon-bar:nth-child(2)').addClass('active');
+      }, function () {
+        $(this).find('.icon-bar:nth-child(2)').removeClass('active');
+      }).on('click', function () {
+        $('.dropdown-menu').fadeToggle();
+        $(this).toggleClass('active');
+      }).on('mouseleave', function () {
+        $('.dropdown-menu').fadeOut();
+        $(this).removeClass('active');
+      })
+    }
+  }
+})(jQuery);
