a1fcfc7229ab6c2e8858eeac80b411b07b7ef046
Senya <suharchuk.dima@gmail.com>
2018-02-16 18:39:37 +0200

    Fixed bug with clonning content in bottom of contacts page

--- sites/all/themes/vcard/templates/views-view--contacts--page.tpl.php
+++ sites/all/themes/vcard/templates/views-view--contacts--page.tpl.php
@@ -28,73 +28,78 @@
 ?>
 
 <div class="<?php print $classes; ?>">
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <?php print $title; ?>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-  <?php if ($header): ?>
-      <div class="view-header">
-        <?php print $header; ?>
-      </div>
-  <?php endif; ?>
+    <div class="list-wrapper large-4 medium-4 columns">
+        <div class="nano">
+            <div class="nano-content">
+              <?php print render($title_prefix); ?>
+              <?php if ($title): ?>
+                <?php print $title; ?>
+              <?php endif; ?>
+              <?php print render($title_suffix); ?>
+              <?php if ($header): ?>
+                  <div class="view-header">
+                    <?php print $header; ?>
+                  </div>
+              <?php endif; ?>
 
-  <?php if ($exposed): ?>
-      <div class="view-filters">
-        <?php print $exposed; ?>
-      </div>
-  <?php endif; ?>
+              <?php if ($exposed): ?>
+                  <div class="view-filters">
+                    <?php print $exposed; ?>
+                  </div>
+              <?php endif; ?>
 
-  <?php if ($attachment_before): ?>
-      <div class="attachment attachment-before">
-        <?php print $attachment_before; ?>
-      </div>
-  <?php endif; ?>
+              <?php if ($attachment_before): ?>
+                  <div class="attachment attachment-before">
+                    <?php print $attachment_before; ?>
+                  </div>
+              <?php endif; ?>
 
-  <?php if ($rows): ?>
-      <div class="nano">
-          <div class="nano-content">
-            <?php print $rows; ?>
-          </div>
-      </div>
-  <?php elseif ($empty): ?>
-      <div class="view-empty">
-        <?php print $empty; ?>
-      </div>
-  <?php endif; ?>
+              <?php if ($rows): ?>
+                  <div class="nano">
+                      <div class="nano-content">
+                        <?php print $rows; ?>
+                      </div>
+                  </div>
+              <?php elseif ($empty): ?>
+                  <div class="view-empty">
+                    <?php print $empty; ?>
+                  </div>
+              <?php endif; ?>
 
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
+              <?php if ($pager): ?>
+                <?php print $pager; ?>
+              <?php endif; ?>
 
-  <?php if ($attachment_after): ?>
-      <div class="attachment attachment-after">
-        <?php print $attachment_after; ?>
-      </div>
-  <?php endif; ?>
+              <?php if ($attachment_after): ?>
+                  <div class="attachment attachment-after">
+                    <?php print $attachment_after; ?>
+                  </div>
+              <?php endif; ?>
 
-  <?php if ($more): ?>
-    <?php print $more; ?>
-  <?php endif; ?>
+              <?php if ($more): ?>
+                <?php print $more; ?>
+              <?php endif; ?>
 
-  <?php if ($footer): ?>
-      <div class="view-footer">
-        <?php print $footer; ?>
-      </div>
-  <?php endif; ?>
+              <?php if ($footer): ?>
+                  <div class="view-footer">
+                    <?php print $footer; ?>
+                  </div>
+              <?php endif; ?>
 
-  <?php if ($feed_icon): ?>
-      <div class="feed-icon">
-        <?php print $feed_icon; ?>
-      </div>
-  <?php endif; ?>
+              <?php if ($feed_icon): ?>
+                  <div class="feed-icon">
+                    <?php print $feed_icon; ?>
+                  </div>
+              <?php endif; ?>
+            </div>
+        </div>
+    </div>
 
-</div>
+    <div class="vcard-view-wrapper large-8 medium-8 columns">
+        <div class="nano">
+            <div class="nano-content">
 
-<div class="vcard-view-wrapper large-8 medium-8 columns">
-    <div class="nano">
-        <div class="nano-content">
-
+            </div>
         </div>
     </div>
 </div>
