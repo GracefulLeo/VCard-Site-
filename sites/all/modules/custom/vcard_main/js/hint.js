5de3cc4d7cb1ac607e3da3b547eb2fd6e79dde2a
Senya <suharchuk.dima@gmail.com>
2018-02-22 11:13:49 +0000

    Removed unnecessary hint functionality

--- sites/all/modules/custom/vcard_main/js/hint.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($) {
-  setTimeout(function () {
-    $('.hint-outer').fadeIn('slow')
-  }, 1000);
-
-  $('.hint-outer').on('click', function (event) {
-    $(event.currentTarget).hide()
-  })
-});
