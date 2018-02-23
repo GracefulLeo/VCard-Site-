46d6efa72ebfdab491003a42beaf4e0c8714eb30
Senya <suharchuk.dima@gmail.com>
2018-02-23 10:49:10 +0200

    Printed groups list on groups page

--- sites/all/themes/vcard/templates/entity-nano-teaser.tpl.php
+++ sites/all/themes/vcard/templates/entity-nano-teaser.tpl.php
@@ -1,5 +1,5 @@
 <div class="list-item list-item-<?php print render($entity_type); ?>"
-     data-id="<?php print render($nid); ?>">
+     data-id="<?php print render($id); ?>">
     <div class="logo-micro">
       <?php print render($image); ?>
     </div>
