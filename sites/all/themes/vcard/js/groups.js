8c413409144ad63ac91903a2b84363f693ba0ba9
Senya <suharchuk.dima@gmail.com>
2018-02-28 01:03:51 +0200

    Fixed bug with click on contacts on group view

--- sites/all/themes/vcard/js/groups.js
+++ sites/all/themes/vcard/js/groups.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.actionArrows = {
     attach: function (context) {
-      $('.contact-wrapper', context).bind('click', function (event) {
+      $(context).on('click', '.contact-wrapper', function (event) {
         $(this).addClass('expanded').siblings().removeClass('expanded');
 
         var icon = $(event.currentTarget).find('.vcard-image').is(':visible') ? 'keyboard_arrow_down' : 'keyboard_arrow_up';
